e05b0595b8c3ee437cefc6f195f3aca1495009bf
siraku <slstc6666@yahoo.co.jp>
2025-08-31 14:31:30 +0900

    modify gemini promot

--- utils/gemini_utiles.py
+++ utils/gemini_utiles.py
@@ -11,7 +11,7 @@ client = genai.Client(api_key=API_KEY)
 def generate_content(subtitles):
     print("Gemini Generating content...")
     response = client.models.generate_content(
-    model="gemini-2.5-flash", contents="请帮忙总结下面的youtube字幕，要求用要点方式。不重要的信息可以忽略掉"+subtitles)
+    model="gemini-2.5-flash", contents="请帮忙总结下面的youtube字幕，要求用要点方式。不重要的信息可以忽略掉,输出语言与字幕语言一致"+subtitles)
     
     return response.text
     
